2657e090da72d82aee1e58cf71e1502a12847899
Takeyuki FUJIOKA <fujioka@rabbix.net>
2021-12-27 04:59:58 +0900

    Initial Commit

---
+++ MyRPN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyRPN
+//
+//  Created by fujioka on 2021/12/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyRPN/MyRPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRPNApp.swift
+//  MyRPN
+//
+//  Created by fujioka on 2021/12/27.
+//
+
+import SwiftUI
+
+@main
+struct MyRPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
